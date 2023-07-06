bc32cf3e58babb1ecd94f07a928b72ebc6e42069
Nicolas ROMANOFF <niromano@paul-f4Br3s1.clusters.42paris.fr>
2023-07-06 13:45:13 +0200

    free clean

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 09:39:06 by niromano          #+#    #+#             */
-/*   Updated: 2023/07/05 12:10:51 by niromano         ###   ########.fr       */
+/*   Updated: 2023/07/06 13:02:42 by niromano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,15 @@
 # include "../libft/libft.h"
 # include "../minilibx-linux/mlx.h"
 # include "../minilibx-linux/mlx_int.h"
+
+typedef struct s_mlx
+{
+	void	*mlx;
+	void	*win;
+	void	*img;
+	int		len_w_x;
+	int		len_w_y;
+	char	*path_case_img;
+}	t_mlx;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,49 +6,73 @@
 /*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 09:42:01 by niromano          #+#    #+#             */
-/*   Updated: 2023/07/06 12:22:58 by niromano         ###   ########.fr       */
+/*   Updated: 2023/07/06 13:44:32 by niromano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-int	exit_end(int key)
+int	input(int key, t_mlx *mlx)
 {
 	if (key == XK_Escape)
 	{
+		mlx_destroy_image(mlx->mlx, mlx->img);
+		mlx_destroy_window(mlx->mlx, mlx->win);
+		mlx_destroy_display(mlx->mlx);
+		free(mlx->mlx);
 		exit(EXIT_SUCCESS);
 	}
 	return (0);
 }
 
-int	main(void)
+void	init_map(t_mlx *mlx, char *map)
 {
-	void	*mlx;
-	void	*img;
-	void	*mlx_win;
-	char	*path_img;
+	char	*s;
+	int		fd;
+	int		x;
+	int		y;
+
+	fd = open(map, O_RDONLY);
+	s = get_next_line(fd);
+	x = ft_strlen(s);
+	y = 0;
+	while (s != NULL)
+	{
+		y ++;
+		free(s);
+		s = get_next_line(fd);
+	}
+	mlx->len_w_x = x * 60;
+	mlx->len_w_y = y * 60;
+	close(fd);
+}
+
+int	main(int argc, char *argv[])
+{
+	t_mlx	mlx;
 	int		x = 0;
 	int		y = 0;
 	int		case_x = 0;
 	int		case_y = 0;
-	
-	path_img = "./image/among2.xpm";
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 800, 600, "So_long");
-	img = mlx_xpm_file_to_image(mlx, path_img, &x, &y);
-	if (img == NULL)
+
+	mlx.path_case_img = "./image/among6.xpm";
+	mlx.mlx = mlx_init();
+	init_map(&mlx, argv[argc - 1]);
+	mlx.win = mlx_new_window(mlx.mlx, mlx.len_w_x, mlx.len_w_y, "So_long");
+	mlx.img = mlx_xpm_file_to_image(mlx.mlx, mlx.path_case_img, &x, &y);
+	if (mlx.img == NULL)
 		ft_printf("Image Failed\n");
-	while (case_y < 600)
+	while (case_x < mlx.len_w_x)
 	{
-		while (case_x < 800)
+		while (case_y < mlx.len_w_y)
 		{
-			mlx_put_image_to_window(mlx, mlx_win, img, case_x, case_y);
-			case_x += 50;
+			mlx_put_image_to_window(mlx.mlx, mlx.win, mlx.img, case_x, case_y);
+			case_y += 60;
 		}
-		case_x = 0;
-		case_y += 50;
+		case_y = 0;
+		case_x += 60;
 	}
-	mlx_hook(mlx_win, KeyPress, KeyPressMask, exit_end, NULL);
-	mlx_loop(mlx);
+	mlx_hook(mlx.win, KeyPress, KeyPressMask, input, &mlx);
+	mlx_loop(mlx.mlx);
 	return (0);
 }
