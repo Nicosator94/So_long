0a68df4e8fe5dc9ee3e8b790cf37c54ea21e3bc7
user <user@VM42.myguest.virtualbox.org>
2023-07-13 20:51:32 +0200

    end

--- srcs/set_img.c
+++ srcs/set_img.c
@@ -6,7 +6,7 @@
 /*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 15:57:55 by niromano          #+#    #+#             */
-/*   Updated: 2023/07/13 17:10:29 by niromano         ###   ########.fr       */
+/*   Updated: 2023/07/13 20:43:03 by niromano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ void	set_img(t_mlx *mlx)
 	int		x;
 	int		y;
 
-	path = "./image/jaune.xpm";
+	path = "./image/ground.xpm";
 	mlx->img_0 = mlx_xpm_file_to_image(mlx->mlx, path, &x, &y);
 	path = "./image/box.xpm";
 	mlx->img_1 = mlx_xpm_file_to_image(mlx->mlx, path, &x, &y);
-	path = "./image/bleu.xpm";
+	path = "./image/wall.xpm";
 	mlx->img_2 = mlx_xpm_file_to_image(mlx->mlx, path, &x, &y);
 	path = "./image/neon_1.xpm";
 	mlx->img_p = mlx_xpm_file_to_image(mlx->mlx, path, &x, &y);
