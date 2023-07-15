1898fd49ba8ab976e4f64c356ed9188e5cc4a5f8
user <user@VM42.myguest.virtualbox.org>
2023-07-15 03:06:06 +0200

    anim spike

--- srcs/animation.c
+++ srcs/animation.c
@@ -6,7 +6,7 @@
 /*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 01:17:20 by niromano          #+#    #+#             */
-/*   Updated: 2023/07/15 02:16:45 by niromano         ###   ########.fr       */
+/*   Updated: 2023/07/15 03:05:13 by niromano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,29 @@
 
 int	animation(t_mlx *mlx)
 {
-	int	x;
-	int	y;
+	int	p_x;
+	int	p_y;
+	int	s_x;
+	int	s_y;
 
-	x = mlx->p_x * 60;
-	y = mlx->p_y * 60;
+	p_x = mlx->p_x * 60;
+	p_y = mlx->p_y * 60;
+	s_x = mlx->spike_x * 60;
+	s_y = mlx->spike_y * 60;
 	if (mlx->timer == 0)
-		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.p_1, x, y);
+		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.p_1, p_x, p_y);
 	else if (mlx->timer == 1500)
-		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.p_2, x, y);
+		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.p_2, p_x, p_y);
 	else if (mlx->timer == 3000)
-		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.p_3, x, y);
-	mlx->timer ++;
-	if (mlx->timer > 4500)
+		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.p_3, p_x, p_y);
+	if (!(p_x == s_x && p_y == s_y))
+	{
+		if (mlx->timer == 0 && mlx->collectibles != 0)
+			mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.e_c, s_x, s_y);
+		else if (mlx->timer == 3500 && mlx->collectibles != 0)
+			mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.e_o, s_x, s_y);
+	}
+	if (mlx->timer++ > 4500)
 		mlx->timer = 0;
 	return (0);
 }
