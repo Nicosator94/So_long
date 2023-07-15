1adfb9310447c86ca9790255a38c80cd8781baa2
user <user@VM42.myguest.virtualbox.org>
2023-07-15 21:51:21 +0200

    timer anim

--- srcs/animation.c
+++ srcs/animation.c
@@ -6,7 +6,7 @@
 /*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 01:17:20 by niromano          #+#    #+#             */
-/*   Updated: 2023/07/15 03:14:21 by niromano         ###   ########.fr       */
+/*   Updated: 2023/07/15 21:48:57 by niromano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ int	animation(t_mlx *mlx)
 	s_y = mlx->spike_y * 60;
 	if (mlx->timer == 0)
 		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.p_1, p_x, p_y);
-	else if (mlx->timer == 1500 || mlx->timer == 4500)
+	else if (mlx->timer == 1000 || mlx->timer == 3000)
 		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.p_2, p_x, p_y);
-	else if (mlx->timer == 3000)
+	else if (mlx->timer == 2000)
 		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.p_3, p_x, p_y);
 	if (!(p_x == s_x && p_y == s_y))
 	{
@@ -36,7 +36,7 @@ int	animation(t_mlx *mlx)
 		else if (mlx->timer == 3000 && mlx->collec != 0)
 			mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.e_c, s_x, s_y);
 	}
-	if (mlx->timer++ > 6000)
+	if (mlx->timer++ > 4000)
 		mlx->timer = 0;
 	return (0);
 }
