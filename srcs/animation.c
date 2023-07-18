d329029111aad5b741289ecb681b3356416a4b11
Nicolas ROMANOFF <niromano@paul-f4Ar5s11.clusters.42paris.fr>
2023-07-18 08:45:49 +0200

    anim or vm

--- srcs/animation.c
+++ srcs/animation.c
@@ -6,7 +6,7 @@
 /*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 01:17:20 by niromano          #+#    #+#             */
-/*   Updated: 2023/07/16 12:46:26 by niromano         ###   ########.fr       */
+/*   Updated: 2023/07/18 08:45:21 by niromano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,18 @@ int	animation(t_mlx *mlx)
 	sy = mlx->spike_y * 60;
 	if (mlx->timer == 0)
 		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.p_1, px, py);
-	else if (mlx->timer == 2500 || mlx->timer == 7500)
+	else if (mlx->timer == 25000 || mlx->timer == 75000)
 		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.p_2, px, py);
-	else if (mlx->timer == 5000)
+	else if (mlx->timer == 50000)
 		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.p_3, px, py);
 	if (!(px == sx && py == sy))
 	{
 		if (mlx->timer == 0 && mlx->collec != 0)
 			mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.e_o, sx, sy);
-		else if (mlx->timer == 5500 && mlx->collec != 0)
+		else if (mlx->timer == 55000 && mlx->collec != 0)
 			mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.e_c, sx, sy);
 	}
-	if (mlx->timer++ > 10000)
+	if (mlx->timer++ > 100000)
 		mlx->timer = 0;
 	return (0);
 }
--- srcs_bonus/animation_bonus.c
+++ srcs_bonus/animation_bonus.c
@@ -6,7 +6,7 @@
 /*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 01:17:20 by niromano          #+#    #+#             */
-/*   Updated: 2023/07/16 14:23:27 by niromano         ###   ########.fr       */
+/*   Updated: 2023/07/18 08:45:31 by niromano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,18 @@ int	animation(t_mlx *mlx)
 	sy = mlx->spike_y * 60;
 	if (mlx->timer == 0 && mlx->map[mlx->p_y][mlx->p_x] == 'P')
 		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.p_1, px, py);
-	else if ((mlx->timer == 2500 || mlx->timer == 7500) && mlx->map[mlx->p_y][mlx->p_x] == 'P')
+	else if ((mlx->timer == 25000 || mlx->timer == 75000) && mlx->map[mlx->p_y][mlx->p_x] == 'P')
 		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.p_2, px, py);
-	else if (mlx->timer == 5000 && mlx->map[mlx->p_y][mlx->p_x] == 'P')
+	else if (mlx->timer == 50000 && mlx->map[mlx->p_y][mlx->p_x] == 'P')
 		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.p_3, px, py);
 	if (!(px == sx && py == sy))
 	{
 		if (mlx->timer == 0 && mlx->collec != 0)
 			mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.e_o, sx, sy);
-		else if (mlx->timer == 5500 && mlx->collec != 0)
+		else if (mlx->timer == 55000 && mlx->collec != 0)
 			mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img.e_c, sx, sy);
 	}
-	if (mlx->timer++ > 10000)
+	if (mlx->timer++ > 100000)
 		mlx->timer = 0;
 	return (0);
 }
